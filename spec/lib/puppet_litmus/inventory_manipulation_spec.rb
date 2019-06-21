c283c6f94fb1f75a6db7569b1676ad92b110520f
sheena <sheena@puppet.com>
2019-06-21 09:31:51 +0100

    (minor fix for addfeaturegroup function)

--- spec/lib/puppet_litmus/inventory_manipulation_spec.rb
+++ spec/lib/puppet_litmus/inventory_manipulation_spec.rb
@@ -54,6 +54,17 @@ RSpec.describe PuppetLitmus::InventoryManipulation do
    { 'name' => 'winrm_nodes', 'nodes' => [] }] }
     end
 
+    let(:empty_feature_hash) do
+      { 'groups' =>
+  [{ 'name' => 'ssh_nodes',
+     'nodes' =>
+     [{ 'name' => 'test.delivery.puppetlabs.net',
+        'config' => { 'transport' => 'ssh', 'ssh' => { 'user' => 'root', 'password' => 'Qu@lity!', 'host-key-check' => false } },
+        'facts' => { 'provisioner' => 'vmpooler', 'platform' => 'centos-5-x86_64' } }],
+     'features' => [] },
+   { 'name' => 'winrm_nodes', 'nodes' => [] }] }
+    end
+
     it 'no matching node, raises' do
       expect { dummy_class.config_from_node(config_hash, 'not.here') }.to raise_error('No config was found for not.here')
     end
@@ -77,6 +88,10 @@ RSpec.describe PuppetLitmus::InventoryManipulation do
 
     it 'write from inventory_hash to inventory_yaml file feature_hash' do
       expect { dummy_class.write_to_inventory_file(feature_hash, inventory_full_path) }.not_to raise_error
+    end
+
+    it 'empty feature exists, and returns hash with feature added' do
+      expect(dummy_class.add_feature_to_group(empty_feature_hash, 'puppet-agent', 'ssh_nodes')).to eq('groups' => [{ 'features' => ['puppet-agent'], 'name' => 'ssh_nodes', 'nodes' => [{ 'config' => { 'ssh' => { 'host-key-check' => false, 'password' => 'Qu@lity!', 'user' => 'root' }, 'transport' => 'ssh' }, 'facts' => { 'platform' => 'centos-5-x86_64', 'provisioner' => 'vmpooler' }, 'name' => 'test.delivery.puppetlabs.net' }] }, { 'name' => 'winrm_nodes', 'nodes' => [] }]) # rubocop:disable Metrics/LineLength: Line is too long
     end
 
     it 'write from inventory_hash to inventory_yaml file no feature_hash' do
