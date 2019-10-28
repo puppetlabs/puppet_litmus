1b17df0878d909c26729e83f6a34b468a8c5a5fc
Florin Dragos <florin@puppet.com>
2019-10-28 16:49:04 +0200

    (FM-8355) Add spec_helper_acceptance

--- lib/puppet_litmus.rb
+++ lib/puppet_litmus.rb
@@ -7,6 +7,7 @@ require 'bolt_spec/run'
 require 'puppet_litmus/inventory_manipulation'
 require 'puppet_litmus/puppet_helpers'
 require 'puppet_litmus/rake_helper'
+require 'puppet_litmus/spec_helper_acceptance'
 
 # Helper methods for testing puppet content
 module PuppetLitmus
