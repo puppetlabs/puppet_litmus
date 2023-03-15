9ff30c308b45132d81b83f2c6fd55b555049694e
Lukas Audzevicius <laudzevicius24@hotmail.com>
2023-03-15 13:12:17 +0000

    Spec test update

--- spec/exe/matrix_from_metadata_v2_spec.rb
+++ spec/exe/matrix_from_metadata_v2_spec.rb
@@ -4,7 +4,13 @@ require 'spec_helper'
 
 RSpec.describe 'matrix_from_metadata_v2' do
   context 'without arguments' do
+    let(:github_output) { Tempfile.new('github_output') }
+    let(:github_output_content) { github_output.read }
     let(:result) { run_matrix_from_metadata_v2 }
+
+    before(:each) do
+      ENV['GITHUB_OUTPUT'] = github_output.path
+    end
 
     it 'run successfully' do
       expect(result.status_code).to eq 0
@@ -12,7 +18,7 @@ RSpec.describe 'matrix_from_metadata_v2' do
 
     it 'generates the matrix' do
       expect(result.stdout).to include('::warning::Cannot find image for Ubuntu-14.04')
-      expect(result.stdout).to include(
+      expect(github_output_content).to include(
         [
           'matrix={',
           '"platforms":[',
@@ -26,15 +32,21 @@ RSpec.describe 'matrix_from_metadata_v2' do
           '}',
         ].join,
       )
-      expect(result.stdout).to include(
-        'spec_matrix={"include":[{"puppet_version":"~> 6.0","ruby_version":2.5},{"puppet_version":"~> 7.0","ruby_version":2.7}]}',
+      expect(github_output_content).to include(
+        'spec_matrix={"include":[{"puppet_version":"~> 7.0","ruby_version":2.7},{"puppet_version":"~> 8.0","ruby_version":3.2}]}',
       )
       expect(result.stdout).to include("Created matrix with 8 cells:\n  - Acceptance Test Cells: 6\n  - Spec Test Cells: 2")
     end
   end
 
   context 'with --exclude-platforms ["ubuntu-18.04"]' do
+    let(:github_output) { Tempfile.new('github_output') }
+    let(:github_output_content) { github_output.read }
     let(:result) { run_matrix_from_metadata_v2({ '--exclude-platforms' => ['ubuntu-18.04'] }) }
+
+    before(:each) do
+      ENV['GITHUB_OUTPUT'] = github_output.path
+    end
 
     it 'run successfully' do
       expect(result.status_code).to eq 0
@@ -43,9 +55,9 @@ RSpec.describe 'matrix_from_metadata_v2' do
     it 'generates the matrix without excluded platforms' do
       expect(result.stdout).to include('::warning::Cannot find image for Ubuntu-14.04')
       expect(result.stdout).to include('::warning::Ubuntu-18.04 was excluded from testing')
-      expect(result.stdout).to include(
+      expect(github_output_content).to include(
         [
-          '::set-output name=matrix::{',
+          'matrix={',
           '"platforms":[',
           '{"label":"CentOS-6","provider":"provision::docker","image":"litmusimage/centos:6"},',
           '{"label":"RedHat-8","provider":"provision::provision_service","image":"rhel-8"}',
@@ -56,15 +68,21 @@ RSpec.describe 'matrix_from_metadata_v2' do
           '}',
         ].join,
       )
-      expect(result.stdout).to include(
-        '::set-output name=spec_matrix::{"include":[{"puppet_version":"~> 7.0","ruby_version":2.7},{"puppet_version":"~> 8.0","ruby_version":3.2}]}',
+      expect(github_output_content).to include(
+        'spec_matrix={"include":[{"puppet_version":"~> 7.0","ruby_version":2.7},{"puppet_version":"~> 8.0","ruby_version":3.2}]}',
       )
       expect(result.stdout).to include("Created matrix with 6 cells:\n  - Acceptance Test Cells: 4\n  - Spec Test Cells: 2")
     end
   end
 
   context 'with --exclude-platforms \'["ubuntu-18.04","redhat-8"]\'' do
+    let(:github_output) { Tempfile.new('github_output') }
+    let(:github_output_content) { github_output.read }
     let(:result) { run_matrix_from_metadata_v2({ '--exclude-platforms' => ['ubuntu-18.04', 'redhat-8'] }) }
+
+    before(:each) do
+      ENV['GITHUB_OUTPUT'] = github_output.path
+    end
 
     it 'run successfully' do
       expect(result.status_code).to eq 0
@@ -74,9 +92,9 @@ RSpec.describe 'matrix_from_metadata_v2' do
       expect(result.stdout).to include('::warning::Cannot find image for Ubuntu-14.04')
       expect(result.stdout).to include('::warning::Ubuntu-18.04 was excluded from testing')
       expect(result.stdout).to include('::warning::RedHat-8 was excluded from testing')
-      expect(result.stdout).to include(
+      expect(github_output_content).to include(
         [
-          '::set-output name=matrix::{',
+          'matrix={',
           '"platforms":[',
           '{"label":"CentOS-6","provider":"provision::docker","image":"litmusimage/centos:6"}',
           '],',
@@ -86,8 +104,8 @@ RSpec.describe 'matrix_from_metadata_v2' do
           '}',
         ].join,
       )
-      expect(result.stdout).to include(
-        '::set-output name=spec_matrix::{"include":[{"puppet_version":"~> 7.0","ruby_version":2.7},{"puppet_version":"~> 8.0","ruby_version":3.2}]}',
+      expect(github_output_content).to include(
+        'spec_matrix={"include":[{"puppet_version":"~> 7.0","ruby_version":2.7},{"puppet_version":"~> 8.0","ruby_version":3.2}]}',
       )
       expect(result.stdout).to include("Created matrix with 4 cells:\n  - Acceptance Test Cells: 2\n  - Spec Test Cells: 2")
     end
