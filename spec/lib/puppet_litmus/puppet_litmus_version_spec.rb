46e5dc0e1ec95a260b6c790a5522492f3bc7643d
Gavin Patton <gavin.patton@puppet.com>
2023-04-06 13:45:45 +0100

    (CONT-806) Correct RSpec/ClassCheck

--- spec/lib/puppet_litmus/puppet_litmus_version_spec.rb
+++ spec/lib/puppet_litmus/puppet_litmus_version_spec.rb
@@ -5,6 +5,6 @@ require 'spec_helper'
 RSpec.describe PuppetLitmus do
   it 'has a version number' do
     expect(described_class::VERSION).not_to be_nil
-    expect(described_class::VERSION).to be_a_kind_of(String)
+    expect(described_class::VERSION).to be_a(String)
   end
 end
