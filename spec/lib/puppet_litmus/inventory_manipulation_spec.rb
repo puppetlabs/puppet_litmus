6a51d061d5111e6871961a5d2698c50eb30505a2
Gavin Patton <gavin.patton@puppet.com>
2023-04-06 13:44:12 +0100

    (CONT-806) Correct RSpec/BeEq

--- spec/lib/puppet_litmus/inventory_manipulation_spec.rb
+++ spec/lib/puppet_litmus/inventory_manipulation_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe PuppetLitmus::InventoryManipulation do
 
   context 'with config_from_node' do
     it 'no matching node, returns nil' do
-      expect(config_from_node(config_hash, 'not.here')).to eq(nil)
+      expect(config_from_node(config_hash, 'not.here')).to be_nil
     end
 
     it 'no config section, returns nil' do
-      expect(config_from_node(no_config_hash, 'test.delivery.puppetlabs.net')).to eq(nil)
+      expect(config_from_node(no_config_hash, 'test.delivery.puppetlabs.net')).to be_nil
     end
 
     it 'config exists, and returns' do
